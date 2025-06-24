e2b1d43073c2e17ac8c0d5bee39cfb0fa6cf7c26
Ainesh Sridhar <aineshsridhar@gmail.com>
2025-06-24 22:04:43 +0530

    Prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home(){
         <div>
             <Navbar/>
              <main className="">
-                <p className="text-5xl font-bold">Find your perfect project or hackathon partner. Connect with people whose skills complement yous and build something amazing together</p>
+                <p className="text-3xl font-bold text-orange-500">Find your perfect project or hackathon partner. Connect with people whose skills complement yous and build something amazing together</p>
                 <a href ="/login" className="">Get Started</a>
             </main>
         </div>
