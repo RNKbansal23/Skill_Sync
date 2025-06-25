c684ec8178703d1fdc8415011d01b75647c45c8d
Ainesh Sridhar <aineshsridhar@gmail.com>
2025-06-25 14:57:45 +0530

    Set up authentication

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,23 @@
+import {NextResponse} from 'next/server'
+import prisma from "@/lib/db"
+import bcrypt from 'bcrypt'
+import { useDynamicRouteParams } from 'next/dist/server/app-render/dynamic-rendering';
+
+export async function POST(request: Request){
+    try{
+        const {email, password} = await request.json()
+        const user = await prisma.user.findUnique({where: {email}});
+        if(!user) {
+            return NextResponse.json({error: 'Invalid Credentials'}, {status: 401})
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid){
+            return NextResponse.json({error: 'Invalid Credentials'}, {status: 401})
+        }
+
+        return NextResponse.json({message: "Login successful", user: {id: user.id, email: user.email}}, {status:200})
+    } catch (error){
+        return NextResponse.json({error: 'Login failed'}, {status: 500})
+    }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Navbar from './components/Navbar';
+import Navbar from '../components/Navbar';
 
 export default function Home(){
     return (
         <div>
             <Navbar/>
-             <main className="">
+             <main className="bg-purple-500">
                 <p className="text-3xl font-bold text-orange-500">Find your perfect project or hackathon partner. Connect with people whose skills complement yous and build something amazing together</p>
                 <a href ="/login" className="">Get Started</a>
             </main>
